6f5ed4433dde4f003e1846031c853e4ecf967431
yewton <yewton@gmail.com>
2023-01-09 23:42:25 +0900

    ✏ SQL の元ネタを書く

--- examples/module-b/b-2.sql
+++ examples/module-b/b-2.sql
@@ -1,3 +1,4 @@
+-- https://github.com/JSQLParser/JSqlParser/blob/f922e0d8efee673fa1d9176d1d4421b549be09ff/src/test/resources/net/sf/jsqlparser/statement/select/oracle-tests/cast_multiset01.sql
 select t1.department_id, t2.*
 from shared_table2 t1,
      table
